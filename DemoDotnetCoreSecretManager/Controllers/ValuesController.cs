862302081007be4e56a1648d72b87da3b567054e
Poy Chang <poypost@gmail.com>
2017-11-05 11:23:05 +0800

    修改 Values 控制器，注入設定的服務

--- DemoDotnetCoreSecretManager/Controllers/ValuesController.cs
+++ DemoDotnetCoreSecretManager/Controllers/ValuesController.cs
@@ -1,15 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DemoDotnetCoreSecretManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DemoDotnetCoreSecretManager.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly AppSettings settings;
+
+        public ValuesController(IOptions<AppSettings> options)
+        {
+            settings = options.Value;
+        }
+
         // GET api/values
         [HttpGet]
         public IActionResult Get()
         {
-            return new EmptyResult();
+            return new JsonResult(settings);
         }
     }
 }
