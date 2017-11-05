4193aeb7b1ea6060dcfc11f6fda7aef65f74342d
Poy Chang <poypost@gmail.com>
2017-11-05 11:21:20 +0800

    建立注入設定的服務

--- DemoDotnetCoreSecretManager/Startup.cs
+++ DemoDotnetCoreSecretManager/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DemoDotnetCoreSecretManager.Models;
 
 namespace DemoDotnetCoreSecretManager
 {
@@ -23,6 +24,8 @@ namespace DemoDotnetCoreSecretManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<AppSettings>(Configuration);
+
             services.AddMvc();
         }
 
